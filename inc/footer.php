0d6622def65f2919d3bd44d7b0ad5febb80c386c
Adriana Salgado Cardenas <salgadoadriana579@gmail.com>
2022-10-31 13:13:14 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,47 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="https://www.facebook.com/IPSdeCorazon" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="https://www.youtube.com/channel/UCf019Mhtm8LsS98Vllg1FOA" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="https://instagram.com/prosalco_ips?igshid=YmMyMTA2M2Y=" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="https://prosalcoips.com/" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Política de protección de datos personales.</br>
+                    Seguridad y salud en el trabajo.</br>
+                    Canal de transparencia</br>
+                    vigilado supersalud.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Sede administrativa</p>
+                <p style="color: #FFF">
+
+Carrera 43 A N° 25 – 41 &nbsp;
+
+Avenida Poblado</p>
+                
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true"> +57 –300 454 41 53</p> &nbsp;&nbsp;<a href="https://wa.me/+57300454415" target="_blank" style="color: #5bc0de">Envianos Un Mensaje</a></br>
+                E-mail: <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blank" style="color: #5bc0de"> &nbsp; atencionalusuario@prosalcoips.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">
+Copyright © 2020 Prosalco IPS | 20sAgencia | Todos los derechos reservados</h5>
+</footer>
