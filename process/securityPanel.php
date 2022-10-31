e159da81bd953b8755fff0f9e941f3ef199afa47
Adriana Salgado Cardenas <salgadoadriana579@gmail.com>
2022-10-31 13:04:59 -0500

    Delete process directory

--- process/securityPanel.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-error_reporting(E_PARSE);
-if ($_SESSION['nombreAdmin']=="") {
-    header("Location: index.php");
-}
--- process/vaciarcarrito.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['carro']);
-?>
-<script>
-    window.location = "../carrito.php";
-</script>
